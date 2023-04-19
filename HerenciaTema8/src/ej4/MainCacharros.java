a10b2c5d1ac58d862b5d3f9e6261cb244cac23f3
Siobhantt <luisa.cardozo@iesnervion.es>
2023-04-19 19:52:15 +0200

    Commit Main

---
+++ HerenciaTema8/src/ej4/MainCacharros.java
@@ -0,0 +1,16 @@
+package ej4;
+
+public class MainCacharros {
+	static Electrodomestico[] arrayCacharros =  new Electrodomestico[10];
+	public static void main(String[] args) {
+		Electrodomestico a1 = new Electrodomestico();
+		Lavadora b1 = new Lavadora();
+		Television c1 =  new Television();
+		Electrodomestico a2 = new Electrodomestico();
+		Lavadora b2 = new Lavadora();
+		Television c2 =  new Television();
+		
+		
+	}
+
+}
