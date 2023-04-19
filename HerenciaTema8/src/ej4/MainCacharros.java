70eace9a93bb7b2f7864d674b9d023b1ca8e8e42
Siobhantt <luisa.cardozo@iesnervion.es>
2023-04-20 00:43:06 +0200

    Probando el main

--- HerenciaTema8/src/ej4/MainCacharros.java
+++ HerenciaTema8/src/ej4/MainCacharros.java
@@ -1,16 +1,39 @@
 package ej4;
 
+import ej4.Electrodomestico.Color;
+import ej4.Electrodomestico.Consumo;
+
 public class MainCacharros {
-	static Electrodomestico[] arrayCacharros =  new Electrodomestico[10];
+	static Electrodomestico[] arrayCacharros = new Electrodomestico[10];
+
 	public static void main(String[] args) {
-		Electrodomestico a1 = new Electrodomestico();
-		Lavadora b1 = new Lavadora();
-		Television c1 =  new Television();
-		Electrodomestico a2 = new Electrodomestico();
-		Lavadora b2 = new Lavadora();
-		Television c2 =  new Television();
+		Electrodomestico a1 = new Electrodomestico(50,10,"gris",'A');
+		Electrodomestico a2 = new Electrodomestico(500,30);
+		Electrodomestico a3 = new Electrodomestico();
+		
+		Lavadora b1 = new Lavadora(400,40);
+		Lavadora b2 = new Lavadora(15,400,35);
+		Lavadora b3 = new Lavadora(40,600,50);
+		Lavadora b4 = new Lavadora();
+		
+		Television c1 = new Television();
+		Television c2 = new Television();
+		Television c3 = new Television();
 		
 		
+		arrayCacharros[0] = a1;
+		arrayCacharros[1] = a2;
+		arrayCacharros[2] = a3;
+
+		arrayCacharros[3] = b1;
+		arrayCacharros[4] = b2;
+		arrayCacharros[5] = b3;
+		arrayCacharros[6] = b4;
+
+		arrayCacharros[7] = c1;
+		arrayCacharros[8] = c2;
+		arrayCacharros[9] = c3;
+
 	}
 
 }
